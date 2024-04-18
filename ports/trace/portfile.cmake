3760260021abcb0d4c714004494512a581d91595
str3jda <str3jda@gmail.com>
2024-04-18 12:08:53 +0200

    .

--- ports/trace/portfile.cmake
+++ ports/trace/portfile.cmake
@@ -16,10 +16,3 @@ vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME "trace")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(
-	INSTALL "${SOURCE_PATH}/LICENSE"
-	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-	RENAME copyright)
