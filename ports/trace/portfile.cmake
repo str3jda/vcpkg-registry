cbb045a0618f8ac5a7c7da51bf252c51378e4c3b
str3jda <str3jda@gmail.com>
2024-04-18 12:20:42 +0200

    .

--- ports/trace/portfile.cmake
+++ ports/trace/portfile.cmake
@@ -5,14 +5,22 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO  str3jda/TraceApp 
-    REF "${VERSION}"
-    SHA512 c3810a868fc84592ea70cf8445ccff2856147ccf
+    REF c3810a868fc84592ea70cf8445ccff2856147ccf
+    SHA512 ""
     HEAD_REF master
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DTRACE_BUILD_SERVER=OFF)
 
 vcpkg_cmake_install()
-vcpkg_fixup_pkgconfig()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "trace")
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+else()
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/trace)
+endif()
+
+vcpkg_fixup_pkgconfig()
