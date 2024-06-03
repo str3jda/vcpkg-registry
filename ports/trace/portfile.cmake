dd54ddaa98afc67770a95408242083a4195e9c4d
str3jda <str3jda@gmail.com>
2024-06-03 13:33:07 +0200

    .

--- ports/trace/portfile.cmake
+++ ports/trace/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO  str3jda/TraceApp 
+    REPO  str3jda/TraceApp
     REF 1.0.0
     SHA512 38c363329a690131b8d8e4c4071d67f253b69cde4568ddbde36ed0cadc7dcf5f935bd3df1aa8007b8a21ef48730705333f2b4978092f3d1943b1afcada635ee5
     HEAD_REF master
