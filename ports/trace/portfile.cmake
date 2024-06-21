34aa55d7553fcfbcf519541220c3c350cf0ffeab
str3jda <str3jda@gmail.com>
2024-06-21 15:55:50 +0200

    .

--- ports/trace/portfile.cmake
+++ ports/trace/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO  str3jda/TraceApp
     REF 1.0.0
-    SHA512 89929de54db981e9cbd8aadc032079ff1c1622ac40c00e4a5e306e026fac7026f6edd39cdd73dbfbac26d6e3f27d0a9134e8bdfa3daeaad10addfbaccdd1d1d1
+    SHA512 d0ee87ed6a2bdf18aab921c47980522ec5738c115f3fbd39f40087ec73398a5a3589c69938370d6730ae7cce54facaa50cb3713d3e634a558040344dd9a34854
     HEAD_REF master
 )
 
@@ -27,3 +27,5 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Licence.txt")
